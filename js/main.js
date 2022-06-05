f1af4f831dc10cbc4520d927ad76ea451fb8f205
EmilianoAlfonsin <emiliano.a.83@gmail.com>
2022-06-04 23:48:51 -0300

    aplicando constructores y mejorando funciones

--- js/main.js
+++ js/main.js
@@ -1,70 +1,108 @@
-//Declarando variables
+// ------ Declarando variables ------
 let producto;
+let cantidad;
+let iniciarCompra;
 let totalCompra = 0;
 let totalPagar = 0;
-let pampers = 1230;
-let huggies = 1250;
-let estrella = 1100;
-let babysec = 980;
 
-//Declarando función ingreso
-function bienvenida () {
-    alert(nombre + ', te damos la bienvenida a Pipu Baby & kids, pañalera y juguetería online');
+// ------ Creando objetos productos ------
 
+class Producto {
+    constructor(nombre, valor, categoria){
+        this.nombre = nombre
+        this.valor = valor
+        this.categoria = categoria
+    }
 }
 
-//Declarando funcion comprar
+const pampersConfort = new Producto ("Pampers confort sec", 1230, "pañales")
+const pampersPremium = new Producto ("Pampers premium care", 1480, "pañales")
+const huggiesAct = new Producto ("Huggies active sec", 1200, "pañales")
+const qsoftPremium = new Producto ("Qsoft Premium", 280, "toallas humedas")
+const pampersSplash = new Producto ("Pampers splash", 250, "toallas humedas")
+
+// ------ Declarando funciones ------
+
+function bienvenida () {
+    alert(nombre + ', te damos la bienvenida a Pipu Baby & kids, pañalera y juguetería online')
+}
+
+function incorrecto(){
+    alert ("La opción seleccionada no es correcta. Por favor intentelo nuevamente.")
+}
+
+function cuantia () {
+    return Number(prompt ("Qué cantidad deseas comprar?"))
+}
+
 function comprar() {
-    producto = prompt ("Que producto estabas buscando? Seleccioná Pampers, Huggies, Estrella o Babysec.").toLowerCase();
+    producto = prompt ("Qué producto estabas buscando? Seleccioná Toallitas o Pañales").toLowerCase()
 
     switch (producto) {
-        case "pampers":
-            alert ("Ha seleccionado pañales Pampers");
-            totalCompra += pampers;
+        case "pañales":
+            producto = prompt("Qué marca preferís: Huggies o Pampers ?").toLowerCase()
+            if (producto === "pampers"){
+                producto = prompt ("Puedes elegir Confort o Premium").toLowerCase()
+                if (producto === "confort"){
+                    cantidad = cuantia()
+                    totalCompra += (pampersConfort.valor*cantidad)
+                }else{
+                    cantidad = cuantia()
+                    totalCompra += (pampersPremium.valor*cantidad)
+                }
+            }if (producto === "huggies") {
+                cantidad = cuantia()
+                totalCompra += (huggiesAct.valor*cantidad)
+            }else{
+                incorrecto()
+                comprar()
+            }
             break;
-        case "huggies":
-            alert ("Ha seleccionado pañales Huggies");
-            totalCompra += huggies;
-            break;
-        case "estrella":
-            alert ("Ha seleccionado pañales Estrella");
-            totalCompra += estrella
-            break;
-        case "babysec":
-            alert ("Ha seleccionado pañales Baybisec");
-            totalCompra += babysec
+        case "toallitas":
+            producto = prompt ("Puedes elegir pampers o qsoft").toLowerCase()
+            if (producto === "pampers"){
+                cantidad = cuantia()
+                totalCompra += (pampersSplash.valor*cantidad)
+            }if (producto === "qsoft"){
+                cantidad = cuantia()
+                totalCompra += (qsoftPremium.valor*cantidad)
+            }else{
+                incorrecto()
+                comprar()
+            }
             break;
         default:
-            alert ("La opción seleccionada no es correcta. Por favor intentelo nuevamente.");
-            comprar();
+            comprar()
             break;
     }
-
 }
 
-//Declarando funcion finalizar compra
+function ingresar(){
+    return iniciarCompra = Number(prompt("Si desea comprar un producto elija #1, si desea salir del programa elija #2"))
+}
+
 function finalizarCompra() {
     pagar = prompt ("Ingrese su metodo de pago: 'E' para efectivo o transferencia, o 'T' para pagos con tarjeta de crédito").toLowerCase();
     
     if (pagar === "e") {
         totalCompra *= 0.9
         totalPagar = totalCompra;
-        alert ("El monto a pagar es $" + totalPagar + ". Gracias por su compra!");
+        alert (`El monto a pagar es $${totalPagar}. Gracias por su compra!`);
     } else if (pagar === "t") {
         totalPagar = totalCompra;
-        alert ("El monto a pagar es $" + totalPagar + ". Gracias por su compra!");
+        alert (`El monto a pagar es $${totalPagar}. Gracias por su compra!`);
     }
 }
 
 
-// Iniciando programa - Main
+// ------ Iniciando programa - Main ------
 
 let nombre = prompt("Hola! Cual es tu nombre?").toUpperCase();
 bienvenida();
-let iniciarCompra = prompt("Si desea comprar un producto elija #1, si desea salir del programa elija #2");
+ingresar();
 
-while (iniciarCompra !== "2") {
-    if (iniciarCompra === "1") {
+while (iniciarCompra !== 2) {
+    if (iniciarCompra === 1) {
         comprar ();
         let continuarCompra = prompt ("Desea continuar comprando? Si/No").toLowerCase();
         if(continuarCompra === "si") {
@@ -73,5 +111,8 @@ while (iniciarCompra !== "2") {
             finalizarCompra();
             iniciarCompra = "2";
         }
+    }else{
+        incorrecto();
+        ingresar();
     }
 }
